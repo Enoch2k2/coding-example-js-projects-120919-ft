bc7d7b32d7945fb07be9a012985b5b0da68d16c6
Enoch2k2 <enoch2k2@hotmail.com>
2020-03-24 13:15:52 -0500

    initial commit and added a lot of stuff

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Movie.create(title: "The Fellowship of the Rings", description: "A hobbit steals a ring from gollum and gollum gets it back and falls into a mountain.")
+
+Movie.create(title: "The two towers", description: "A tale of two towers and how they never went anywhere because they were towers.")
+
+Movie.create(title: "The return of the King", description: "The king returns.")
